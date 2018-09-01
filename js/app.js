a3e209c2fe6eccb634b2e643dc81e14a88152699
Curtis Wilson <curtiswemail@gmail.com>
2018-09-01 10:07:03 -0600

    Add player object. Add more properties to enemy object

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -14,6 +17,13 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    if (this.x > 510){
+      this.x = 0;
+      this.speed = 100 + Math.floor(Math.random() * 300)
+    }
+
+    this.x += this.speed * dt;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -25,11 +35,40 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function(x,y,speed){
+  this.x = x;
+  this.y = y;
+  this.speed = speed;
+  this.sprite = 'images/char-cat-girl.png';
+};
+
+Player.prototype.update = function(dt){
+
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(){
+
+};
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var allEnemies = [];
+var player = new Player(200,400,50);
+
+
+var enemySpawn = [60,140,220];
+
+enemySpawn.forEach(function(yCord){
+  var enemy = new Enemy(0, yCord, 100 + Math.floor(Math.random() * 300));
+  allEnemies.push(enemy);
+});
 
 
 // This listens for key presses and sends the keys to your
